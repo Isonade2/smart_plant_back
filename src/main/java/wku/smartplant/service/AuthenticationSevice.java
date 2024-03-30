186dbf6ea118730c785c92009d110f2e0564cbf2
Isonade2 <c1004sos@naver.com>
2024-03-30 20:15:11 +0900

    feat: 로그인 확인 서비스, 응답 빌더 작성

---
+++ src/main/java/wku/smartplant/service/AuthenticationSevice.java
@@ -0,0 +1,25 @@
+package wku.smartplant.service;
+
+import org.springframework.stereotype.Service;
+import wku.smartplant.config.SecurityUtil;
+@Service
+public class AuthenticationSevice {
+    //로그인 여부 확인
+    public Long isLoggedIn(){
+        try{
+            return SecurityUtil.getCurrentMemberId();
+        } catch (Exception e){
+            throw new RuntimeException("로그인이 필요합니다.");
+        }
+    }
+
+    public void IsNotLoggedIn(){
+        try{
+            SecurityUtil.getCurrentMemberId();
+        } catch (Exception e){
+            return;
+        }
+        throw new RuntimeException("이미 로그인 되어있습니다.");
+    }
+
+}
