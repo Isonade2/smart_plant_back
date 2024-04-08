98ce011a8880c12360837b34dfc73a041ad502fd
nemokoala <fdxguy2@gmail.com>
2024-04-09 04:00:07 +0900

    fix: AllArgsConstructor 누락으로 인한 에러 수정

--- src/main/java/wku/smartplant/domain/PlantHistory.java
+++ src/main/java/wku/smartplant/domain/PlantHistory.java
@@ -2,6 +2,7 @@ package wku.smartplant.domain;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +12,7 @@ import wku.smartplant.dto.plant.PlantHistoryDTO;
 @RequiredArgsConstructor
 @Getter
 @Builder
+@AllArgsConstructor
 public class PlantHistory extends BaseTimeEntity {
     @Id @GeneratedValue
     @Column(name = "plant_history_id")
