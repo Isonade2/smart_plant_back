7c11fc852630d8079dc0bae45089139de45147d2
Isonade2 <c1004sos@naver.com>
2024-05-14 13:32:06 +0900

    feat: QuestAPI Swagger Operation 추가

--- src/main/java/wku/smartplant/controller/QuestController.java
+++ src/main/java/wku/smartplant/controller/QuestController.java
@@ -1,11 +1,16 @@
 package wku.smartplant.controller;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import wku.smartplant.dto.ResponseDTO;
 import wku.smartplant.dto.ResponseEntityBuilder;
 import wku.smartplant.dto.quest.*;
 import wku.smartplant.jwt.SecurityUtil;
@@ -20,8 +25,12 @@ import java.util.List;
 public class QuestController {
     private final QuestService questService;
 
+    @Operation(summary = "주간 퀘스트 조회",
+            description = "주간 퀘스트 목록을 조회한다." +
+                    "accept : 퀘스트 수락여부, completed : 퀘스트 완료 여부"
+    )
     @GetMapping("/weekly")
-    public ResponseEntity<?> getWeeklyQuest(){
+    public ResponseEntity<ResponseDTO<List<QuestListDTO>>> getWeeklyQuest(){
         log.info("getWeeklyQuest");
         Long currentMemberId = SecurityUtil.getCurrentMemberId();
         //Long currentMemberId = 52L;
@@ -29,8 +38,15 @@ public class QuestController {
         return ResponseEntityBuilder.build("주간 퀘스트 조회 성공", HttpStatus.OK, weeklyQuest);
     }
 
+
+    @Operation(summary = "퀘스트 수락",
+            description = "주간 퀘스트를 수락한다."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "퀘스트 수락 성공"),
+    })
     @PostMapping("/weekly/accept")
-    public ResponseEntity<?> acceptWeeklyQuest(@RequestBody QuestAcceptRequestDTO requestDTO){
+    public ResponseEntity<ResponseDTO<QuestAcceptResponseDTO>> acceptWeeklyQuest(@RequestBody QuestAcceptRequestDTO requestDTO){
         log.info("acceptWeeklyQuest questId : {}",requestDTO.getQuestId());
         Long currentMemberId = SecurityUtil.getCurrentMemberId();
 //        Long currentMemberId = 52L;
@@ -40,8 +56,15 @@ public class QuestController {
     }
 
     //퀘스트 완료 API
+    @Operation(summary = "퀘스트 완료",
+            description = "주간 퀘스트를 완료한다."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "퀘스트 완료 성공"),
+            @ApiResponse(responseCode = "400", description = "퀘스트 완료 실패")
+    })
     @PostMapping("/weekly/complete")
-    public ResponseEntity<?> completeWeeklyQuest(@RequestBody QuestCompleteRequestDTO requestDTO){
+    public ResponseEntity<ResponseDTO<?>> completeWeeklyQuest(@RequestBody QuestCompleteRequestDTO requestDTO){
         log.info("completeWeeklyQuest questId : {}",requestDTO.getQuestId());
         Long currentMemberId = SecurityUtil.getCurrentMemberId();
         //Long currentMemberId = 52L;
